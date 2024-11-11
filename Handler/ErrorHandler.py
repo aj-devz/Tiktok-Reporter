9d17c955bb0144267a5d9ee2184557902631c775
Aj's Dev <160000744+aj-devz@users.noreply.github.com>
2024-11-11 20:27:04 +0800

    Update ErrorHandler.py

--- Handler/ErrorHandler.py
+++ Handler/ErrorHandler.py
@@ -1,8 +1,14 @@
 from Static.Values import StaticValues
+
 class Handler:
-    def integer_handler(question,min=0,max=0):
+    @staticmethod
+    def integer_handler(question, min=0, max=0):
         while True:
             try:
-                return int(input(f"{question} "))
+                value = int(input(f"{question} "))
+                if value < min or (max != 0 and value > max):
+                    print(f"{StaticValues.WARNING} Input must be between {min} and {max}.")
+                else:
+                    return value
             except ValueError:
-                print(f"{StaticValues.WARNING} Invalid Input, please enter a valid Integer!")
+                print(f"{StaticValues.WARNING} Invalid input, please enter a valid integer!")
